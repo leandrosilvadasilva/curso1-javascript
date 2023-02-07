95c5f040bac8c818247375c7082dd41d7dc74dd2
Leandro Silva da Silva <leandro.tecnicoinfo@gmail.com>
2023-02-07 14:52:59 -0300

    iniciando estudo javascript 2023

---
+++ variaveis.js
@@ -0,0 +1,27 @@
+let idade = 5;
+console.log(idade);
+
+let nome = 'Leandro';
+console.log(nome);
+
+let valorDoIngresso = 20;
+
+let estaAprovado = true;
+let sobrenome;
+let corSelecionada = null;
+
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'de Souza'
+};
+
+console.log(pessoa);
+
+// arrays
+
+let familia = [true, 26, 58, 48, 43];
+
+console.log(familia.length);
+console.log(familia[0])
