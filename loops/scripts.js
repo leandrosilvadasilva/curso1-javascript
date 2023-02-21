0185f460af8123da32d98ec21e754dc9660d0834
Leandro Silva da Silva <leandro.tecnicoinfo@gmail.com>
2023-02-21 09:47:37 -0300

    Incluindo loops

---
+++ loops/scripts.js
@@ -0,0 +1,34 @@
+// for
+/*
+for (let i = 0; i <= 5; i++) {
+    //console.log('Estou aprendendo', i)
+    if (i % 2 !== 0)
+        console.log(i);
+}
+*/
+
+// While
+/*
+let i = 5;
+
+while (i >= 1) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+    i--;
+
+}
+*/
+
+//do while
+/*
+let i = 0;
+do {
+    console.log('digitando!', i);
+    i++;
+
+}
+while (i < 10)
+*/
+
+// for in
